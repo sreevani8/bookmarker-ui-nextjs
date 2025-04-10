5313a321fe88e24fa93a6dc458fd32f3111fcc36
sreevani8 <sreevanidumpala@gmail.com>
2025-04-10 13:21:05 +0530

    docker related commit

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig = {
   /* config options here */
   reactStrictMode: true,
   swcMinify: true,
+
+  serverRuntimeConfig: {
+    API_BASE_URL: process.env.SERVER_SIDE_API_BASE_URL
+  },
+
+  publicRuntimeConfig: {
+    API_BASE_URL: process.env.CLIENT_SIDE_API_BASE_URL
+  },
   async redirects() {
     return [
       {
--- services/api.ts
+++ services/api.ts
@@ -1,14 +1,25 @@
 import axios from "axios"
 import {BookmarksResponse} from "./models";
+import getConfig from "next/config";
+const {serverRuntimeConfig, publicRuntimeConfig} = getConfig()
+//const API_BASE_URL = "http://localhost:8080";
 
-
-const API_BASE_URL = "http://localhost:8080";
+const getApiUrl = () => {
+    return serverRuntimeConfig.API_BASE_URL || publicRuntimeConfig.API_BASE_URL;
+}
 
 export const fetchBookmarks = async (page: number, query: string): Promise<BookmarksResponse> => {
-    let url = `${API_BASE_URL}/api/bookmarks?page=${page}`;
+    console.log("serverRuntimeConfig:", serverRuntimeConfig);
+    console.log("publicRuntimeConfig" , publicRuntimeConfig);
+
+    let url = `${getApiUrl()}/api/bookmarks?page=${page}`
     if(query) {
         url += `&query=${query}`;
     }
     const res = await axios.get<BookmarksResponse>(url)
+    return res.data
+}
+export const saveBookmark = async (bookmark:{title:string , url: string}) => {
+    const res = await axios.post<BookmarksResponse>(`${getApiUrl()}/api/bookmarks`, bookmark)
     return res.data
 }
