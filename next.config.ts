0087376516e3859cbc806d39bede466605a616a3
sreevani8 <sreevanidumpala@gmail.com>
2025-04-11 10:50:52 +0530

    code related commit

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,27 @@
-/** import type { NextConfig } from "next"; */
+/** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV !== 'production';
 
 const nextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  swcMinify: true,
-
   serverRuntimeConfig: {
-    API_BASE_URL: process.env.SERVER_SIDE_API_BASE_URL
+    API_BASE_URL:
+        process.env.SERVER_SIDE_API_BASE_URL ||
+        (isDev ? 'http://localhost:8080' : 'http://bookmarker-api:8080'),
   },
-
   publicRuntimeConfig: {
-    API_BASE_URL: process.env.CLIENT_SIDE_API_BASE_URL
+    API_BASE_URL:
+        process.env.CLIENT_SIDE_API_BASE_URL ||
+        (isDev ? 'http://localhost:8080' : 'http://localhost:18080'),
   },
+/**
+const nextConfig = {
+  reactStrictMode: true,
+  serverRuntimeConfig: {
+    API_BASE_URL: process.env.SERVER_SIDE_API_BASE_URL || 'http://bookmarker-api:8080',
+  },
+  publicRuntimeConfig: {
+    API_BASE_URL: process.env.CLIENT_SIDE_API_BASE_URL || 'http://localhost:18080',
+  },*/
+
   async redirects() {
     return [
       {
@@ -21,7 +31,6 @@ const nextConfig = {
       }
     ]
   }
-
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
